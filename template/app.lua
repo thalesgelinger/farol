c0b8a641d9104b4748afbd10ed4f016f5cdd0fef
Thales Gelinger <thales.gelinger@em.com>
2024-12-23 15:41:12 -0300

    feat: add farol deps

--- template/app.lua
+++ template/app.lua
@@ -1,8 +1,3 @@
--- DEV ONLY
-package.path = package.path .. ";../?.lua"     -- For individual Lua files
-package.path = package.path .. ";./?.lua"      -- For individual Lua files
-package.path = package.path .. ";./?/init.lua" -- For directories containing an init.lua
-
 local app = require "core.router"
 
 app:listen(3000)
